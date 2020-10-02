33b2fbaf6ece547f795ddc80da263d4aa523c359
fcastell-iesemili <fcp00@iesemilidarder.com>
2020-10-02 22:31:35 +0200

    Projecte

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Information about players. Array with example data 
+let players = [
+	["43127678F", "Juan", "Pérez Gómez", "692403829", "jperez@gmail.com", "12/12/1980", "ES6621000418401234567891", "PRO"],
+	["43125043G", "Adolfo", "Gutiérrez Lorma", "692403829", "agutierrez@gmail.com", "12/12/1985", "ES6000491500051234567892", "PRO"],
+	["43125430I", "Carles", "Vich Sacelo", "692403829", "cvich@gmail.com", "12/12/1981", "ES9420805801101234567891", "PRO"],
+	["43124345J", "Gustavo", "Cander More", "692403829", "gmore@gmail.com", "12/12/1995", "ES9000246912501234567891", "PRO"],
+	["43127678F", "Alicia", "Pérez Gómez", "692403829", "jperez@gmail.com", "12/12/1980", "ES6621000418401234567891", "BEG"],
+	["43125043G", "David", "Gutiérrez Lorma", "692403829", "agutierrez@gmail.com", "12/12/1985", "ES6000491500051234567892", "BEG"],
+	["43125430I", "Patricia", "Vich Lorem", "692403829", "cvich@gmail.com", "12/12/1981", "ES9420805801101234567891", "BEG"],
+	["43124345J", "Pepa", "Vivancos Leia", "692403829", "pleia@gmail.com", "12/12/1995", "ES9000246912501234567891", "BEG"]
+];
+
+// Containers and global objects 
+const containerPlayersBeg = document.getElementById('c_beginner_list');
+const containerPlayersPro = document.getElementById('c_professional_list');
+const buttonAdd = document.getElementById('btn-enviar');
+
+init();
+
+//TODO
+// Main function. 
+function init() {}
+
+//TODO
+// This function adds a new player on the list
+/******************************************************************************/
+function addPlayer() {}
+
+//TODO
+// Checks data from the form fields
+function validateForm(){}
+
+//TODO
+// Create two separate lists depens on category. it uses two different containers
+function populateListPlayers() {}
+
+//TODO
+// This function returns true whether the player is 16 years old (for beginners) or 18 years old (for professionals)
+function isValidAgePlayer(sDate, minAge) {}
+
+
